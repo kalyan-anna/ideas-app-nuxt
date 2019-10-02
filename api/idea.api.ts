385094191e1a61833b9018a2b688511a8598bf39
Kalyan <kalyanasundaramannam@nbnco.com.au>
2019-10-03 09:26:37 +0930

    connect with firebase

--- api/idea.api.ts
+++ api/idea.api.ts
@@ -1,8 +1,16 @@
 import { Idea } from '../models/idea';
+import axios from 'axios';
 
 export const registerIdea = async (newIdea: Idea): Promise<Idea> => {
-  newIdea.id = '1';
-  return new Promise<Idea>(resolve => {
-    resolve(newIdea);
-  });
+  const response = await axios.post(
+    'https://idea-app-ed487.firebaseio.com/ideas.json',
+    {
+      ...newIdea
+    }
+  );
+
+  return {
+    ...newIdea,
+    id: response.data.name
+  };
 };
