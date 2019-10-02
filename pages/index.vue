b28075b16c20079eef2ddbb711a2ba8552bef03d
Kalyan <kalyanasundaramannam@nbnco.com.au>
2019-10-03 02:45:11 +0930

    fix alignment

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,10 @@
   <div class="container-fluid layout__container">
     <Header></Header>
     <main class="main-body">
-      <b-button pill variant="primary">Search Ideas</b-button>
-      <b-button pill variant="primary">Register New Idea</b-button>
+      <div class="home__nav">
+        <b-button pill variant="warning">Search ideas</b-button>
+        <b-button pill variant="warning">Register new idea</b-button>
+      </div>
     </main>
     <Footer></Footer>
   </div>
@@ -23,3 +25,30 @@ import Footer from "@/components/layouts/Footer.vue";
 export default class extends Vue {}
 </script>
 
+<style scoped lang="scss">
+@import "~bootstrap/scss/_functions.scss";
+@import "~bootstrap/scss/_variables.scss";
+@import "~bootstrap/scss/mixins/_breakpoints.scss";
+
+.main-body {
+  flex-grow: 2;
+  flex-basis: 100px;
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .home__nav {
+    display: flex;
+    flex-direction: column;
+    @include media-breakpoint-up(md) {
+      flex-direction: row;
+    }
+
+    button {
+      margin: 10px;
+    }
+  }
+}
+</style>
