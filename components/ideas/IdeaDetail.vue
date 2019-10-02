fda705d86245cf136bf93e97e76d07f8a0480e2f
Kalyan <kalyanasundaramannam@nbnco.com.au>
2019-10-03 08:47:53 +0930

    idea view

---
+++ components/ideas/IdeaDetail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-jumbotron :header="idea.title" :lead="idea.previewText">
+      <p>Authour: {{idea.author}}</p>
+      <div>{{ idea.content }}</div>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+import { Idea } from "../../models/idea";
+
+@Component({})
+export default class IdeaDetail extends Vue {
+  @Prop({ type: Object })
+  public idea!: Idea;
+}
+</script>
---
+++ components/ideas/IdeaList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <IdeaDetail v-for="idea in ideas" :key="idea.id" :idea="idea"></IdeaDetail>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator";
+import { Idea } from "../../models/idea";
+import IdeaDetail from "./IdeaDetail.vue";
+
+@Component({
+  components: {
+    IdeaDetail
+  }
+})
+export default class IdeaList extends Vue {
+  @Prop({ type: Array })
+  ideas!: Idea[];
+}
+</script>
